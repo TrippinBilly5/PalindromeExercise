0331208660c8a525f83b6f67913ee14d966cf0a3
mark aldridge <mark.aldridge1@protonmail.com>
2024-06-20 15:40:37 -0700

    Palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var newWord = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                newWord += word[i];
+            }
+
+            return (newWord == word);
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void Test1(string word, bool expected)
         {
-
+            //Arrange
+            var ws = new WordSmith();
+            //Act
+            bool actual = ws.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
